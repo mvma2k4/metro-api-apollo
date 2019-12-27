a9afaa581cb03c28e0eb8733eb077a13ca7cdc91
Marcel Morales <mvma2k4@yahoo.com>
2019-12-26 23:08:49 -0400

    Fix start issues

--- src/server/index.ts
+++ src/server/index.ts
@@ -66,7 +66,7 @@ export async function bootstrap(): Promise<void> {
         });
 
         // Start the server
-        const { url } = await server.listen(4000);
+        const { url } = await server.listen(process.env.PORT || 4000);
         console.log(`Server is running, GraphQL Playground available at ${url}`);
     } catch (err) {
         console.error(err);
